81af8d6eda49068c864f6cfcf5253ce8beeb42d0
Massimo <massimo.rosas279@gmail.com>
2020-12-11 12:53:26 +0100

    Set random sequence

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-import Vue from 'vue'
-import './style.css';
+let vm = new Vue({
+  el: '#app',
+  data: {
+    hautGauche: false,
+    hautDroite: false,
+    basGauche: false,
+    basDroite: false,
+    sequence: [],
+    squareMapping: ['hautGauche', 'hautDroite', 'basGauche', 'basDroite']
+  },
+  methods: {
+    addNewElemToSequence(){
+      this.sequence.push(this.squareMapping[Math.floor(Math.random() * 4)])
+    },
+    newGame(){
+      this.addNewElemToSequence();
+    }
+  }
+})
