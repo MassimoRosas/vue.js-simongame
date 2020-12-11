d65a944878eb672b72e4d9320f625a7c8f4cb498
Massimo <massimo.rosas279@gmail.com>
2020-12-11 12:29:09 +0100

    Set game logic

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ let vm = new Vue({
     basGauche: false,
     basDroite: false,
     sequence: [],
+    tmp: [],
     squareMapping: ['hautGauche', 'hautDroite', 'basGauche', 'basDroite']
   },
   methods: {
     addNewElemToSequence(){
-      this.sequence.push(this.squareMapping[Math.floor(Math.random() * 4)])
+      this.sequence.push(this.squareMapping[Math.floor(Math.random() * 4)]);
+      this.tmp = this.sequence.slice();
     },
     allGray(){
       this.hautGauche = false,
@@ -20,15 +22,40 @@ let vm = new Vue({
     },
     newGame(){
       this.sequence = [];
+      this.nextTurn();
+    },
+    nextTurn(){
       this.addNewElemToSequence();
-      this[this.sequence[0]] = true;
-
+      this.allGray();
+      this.playSequence(this.tmp[0]);
+    },
+    playSequence(instruction){
+      this[instruction] = true;
       setTimeout(function(){
         vm.allGray();
-      }, 300)
+        vm.tmp.shift();
+        if (vm.tmp[0]) {
+          setTimeout(function() {
+            vm.playSequence(vm.tmp[0]);
+          }, 400)
+        } else {
+          vm.tmp = vm.sequence.slice();
+        }
+      }, 400);
     },
     selectSquare(instruction){
-      console.log(instruction);
+      if (instruction === this.tmp[0]) {
+        this[instruction] = true;
+        setTimeout(function(){
+          vm.allGray();
+          vm.tmp.shift();
+          if (!vm.tmp[0]){
+            vm.nextTurn();
+          }
+        }, 400)
+      } else {
+        alert('Perdu !');
+      }
     }
   }
 })
